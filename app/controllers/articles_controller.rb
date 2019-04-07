68f91650180d10ab1545cd07948b41153aab908f
karthikm3427 <karthikm3427@gmail.com>
2019-04-07 20:22:45 +0530

    made partials of comment form and output, put in created_at to display time created of Article and comment, added HTTP security - not to be used on REAL WEB APPS!

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+	http_basic_authenticate_with name: "karthik", password: "12345", 
+	except: [:index, :show]
 	
 	def index
 		@articles = Article.all
