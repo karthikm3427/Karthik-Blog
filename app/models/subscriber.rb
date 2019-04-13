d88b8e392fb0616da8133884e3e7ec4e49b5282d
karthikm3427 <karthikm3427@gmail.com>
2019-04-13 17:47:51 +0530

    Created Model controller subscribers Routessignup and edited css files.

---
+++ app/models/subscriber.rb
@@ -0,0 +1,6 @@
+class Subscriber < ApplicationRecord
+	validates :f_name, :l_name, :email, :country,  
+	presence: true
+	          
+    
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_07_123343) do
+ActiveRecord::Schema.define(version: 2019_04_12_194939) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,15 @@ ActiveRecord::Schema.define(version: 2019_04_07_123343) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "subscribers", force: :cascade do |t|
+    t.string "f_name"
+    t.string "l_name"
+    t.string "email"
+    t.string "country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
